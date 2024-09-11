923632c6a42f5b306fe3eb0c4454afe30490d2af
siqjulia <jusiqueira@davidson.edu>
2024-09-11 00:31:14 -0400

    Figma file added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,11 @@
 
 /* javascript */
 
-var bauhaus = `
+let screenLog = document.querySelector("#screen-log");
+document.addEventListener("mousemove", logKey);
 
-Staaatliches Bauhaus (1919-1933)
-
-      ..-""T-..
-    ,'__   |   '.
-   /   N|  |     !
-  :     '  |      :
-  [      __|      ]
-  :      _I       :
-   !   ___I      /
-    '.  N]     ,'
-      "-"!..--"
-
-`;
-
-console.log(bauhaus);
+function logKey(e) {
+  screenLog.innerText = `
+    Screen X/Y: ${e.screenX}, ${e.screenY}
+    Client X/Y: ${e.clientX}, ${e.clientY}`;
+}
